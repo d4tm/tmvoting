e7d9189a50b00095b141795277a0837ac2d82043
David Singer <david@dsinger.com>
2015-02-18 22:13:07 -0800

    Cleanup.

--- verify.py
+++ verify.py
@@ -23,6 +23,9 @@ opener = urllib2.build_opener(handler)
 
 # For this round, we're going to ignore the fields and use hardcoded values
 
+ecount = json.load(opener.open(baseurl + formname + '/entries/count.json'))['EntryCount']
+ecount = int(ecount)
+
 
 fvote = 'Field6'
 fvalidation = 'Field8'
@@ -30,31 +33,46 @@ fname = 'Field10'
 femail = 'Field13'
 fields = (fvote, fvalidation, fname, femail)
 
-entries = json.load(opener.open(baseurl + formname + '/entries.json'))['Entries']
-
+enext = 0
+pagesize = 100
 newvoters = set()
-for e in entries:
-    (vote, validation, name, email) = [e[f] for f in fields]
-    c.execute('SELECT first, last, title, area, division, email, vote, confirmed FROM voters WHERE validation=?', (validation,))
-    res = c.fetchone()
-    if res:
-        (first, last, title, area, division, realemail, oldvote, confirmed) = res
-        print '%s %s%s %s %s votes %s' % (title, division, area, first, last, vote)
-        if realemail.lower() <> email.lower():
-            print 'email mismatch: db has %s, entered %s' % (realemail, email)
-        if oldvote and vote <> oldvote:
-            print 'vote change from %s to %s' % (oldvote, vote)
-        c.execute('UPDATE voters SET vote = ? WHERE validation=?', (vote, validation))
-        if not confirmed:
-          newvoters.add(email.lower())
-          newvoters.add(realemail.lower())
-    else:
-        print 'Fail!', name, email, validation
+badvoters = set()
 
-vfile = open('newvoters.txt', 'w')
-for v in newvoters:
-    vfile.write(v)
-    vfile.write('\n')
-vfile.close()
+while enext <= ecount:
+
+    entries = json.load(opener.open(baseurl + formname + '/entries.json?pageStart=%d&pageSize=%d' % (enext, pagesize)))['Entries']
+    enext += pagesize
+
+    for e in entries:
+        (vote, validation, name, email) = [e[f] for f in fields]
+        c.execute('SELECT first, last, title, area, division, email, vote, confirmed FROM voters WHERE validation=?', (validation,))
+        res = c.fetchone()
+        if res:
+            (first, last, title, area, division, realemail, oldvote, confirmed) = res
+            if confirmed and oldvote == vote:
+                continue  # Ignore votes already registered
+            print '%s %s%s %s %s votes %s' % (title, division, area, first, last, vote)
+            if realemail.lower() <> email.lower():
+                print 'email mismatch: db has %s, entered %s' % (realemail, email)
+            if oldvote and vote <> oldvote:
+                print 'vote change from %s to %s' % (oldvote, vote)
+            c.execute('UPDATE voters SET vote = ? WHERE validation=?', (vote, validation))
+            if not confirmed:
+              newvoters.add(email.lower())
+              newvoters.add(realemail.lower())
+              badvoters.discard(email.lower())
+              badvoters.discard(realemail.lower())
+        else:
+            print 'Fail!', name, email, validation
+            badvoters.add(email.lower())
+
 conn.commit()
 conn.close()
+vfile = open('newvoters.txt', 'w')
+vfile.write('\n'.join(newvoters))
+vfile.write('\n')
+vfile.close()
+vfile = open('badvoters.txt', 'w')
+vfile.write('\n'.join(badvoters))
+vfile.write('\n')
+vfile.close()
