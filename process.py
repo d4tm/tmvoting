318b54840551d4b4df2134625de680927d9c8294
David Singer <david@dsinger.com>
2015-02-20 13:54:18 -0800

    Major changes to use the YML files and create and send messages

--- process.py
+++ process.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python
-import urllib2, json, sqlite3
-apicode = "QGEF-ADMP-34XV-DR9H"
-formname = "z30733o0zq9upo"
-baseurl = "https://district4tm.wufoo.com/api/v3/forms/"
+import urllib2, json, sqlite3, yaml, smtplib, sys
+from email.mime.text import MIMEText
+try:
+    info = yaml.load(open(sys.argv[1],'r'))
+except IndexError:
+    print 'usage: %s configuration_file' % sys.argv[0]
+    sys.exit(1)
+except yaml.YAMLError, exc:
+    print 'Error in configuration file:', exc
+    sys.exit(2)
 
-# Load the validation codes
-conn = sqlite3.connect('dec.db')
+
+# Connect to the database
+conn = sqlite3.connect(info['db'])
 c = conn.cursor()
 
 # Create a password manager
+baseurl = info['baseurl'] + '/api/v3/forms/'
+formurl = baseurl + info['formname']
 password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-password_mgr.add_password(None, baseurl, apicode, 'footastic')
+password_mgr.add_password(None, baseurl, info['apicode'], 'footastic')
 handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+
+# Connect to the mail server
+info['s'] =  smtplib.SMTP(info['mailserver'])
+info['s'].login(info['from'], info['mailpw'])
 
 # Create "opener"
 opener = urllib2.build_opener(handler)
 
-#fields = opener.open(baseurl + formname + '/fields.json').read()
+#fields = opener.open(formurl + '/fields.json').read()
 #fields = json.loads(fields)['Fields']
 #print fields
 #print len(fields)
 
 # For this round, we're going to ignore the fields and use hardcoded values
 
-ecount = json.load(opener.open(baseurl + formname + '/entries/count.json'))['EntryCount']
+ecount = json.load(opener.open(formurl + '/entries/count.json'))['EntryCount']
 ecount = int(ecount)
 
 
@@ -35,44 +48,80 @@ fields = (fvote, fvalidation, fname, femail)
 
 enext = 0
 pagesize = 100
-newvoters = set()
-badvoters = set()
+newvoters = {}
+badvoters = {}
 
 while enext <= ecount:
 
-    entries = json.load(opener.open(baseurl + formname + '/entries.json?pageStart=%d&pageSize=%d' % (enext, pagesize)))['Entries']
+    entries = json.load(opener.open(formurl + '/entries.json?pageStart=%d&pageSize=%d' % (enext, pagesize)))['Entries']
     enext += pagesize
 
     for e in entries:
         (vote, validation, name, email) = [e[f] for f in fields]
         c.execute('SELECT first, last, title, area, division, email, vote, confirmed FROM voters WHERE validation=?', (validation,))
         res = c.fetchone()
+        # TODO: Handle multiple votes for the same person
         if res:
             (first, last, title, area, division, realemail, oldvote, confirmed) = res
             if confirmed and oldvote == vote:
                 continue  # Ignore votes already registered
-            print '%s %s%s %s %s votes %s' % (title, division, area, first, last, vote)
-            if realemail.lower() <> email.lower():
+            role = ' '.join(('%s %s%s %s %s' % (title, division, area, first, last)).split())
+            print role, 'votes', vote
+            # Normalize email addresses
+            realemail = realemail.lower()
+            email = email.lower()
+            
+            if realemail <> email:
                 print 'email mismatch: db has %s, entered %s' % (realemail, email)
             if oldvote and vote <> oldvote:
                 print 'vote change from %s to %s' % (oldvote, vote)
+                confirmed = False  # Need to confirm any changes
             c.execute('UPDATE voters SET vote = ? WHERE validation=?', (vote, validation))
             if not confirmed:
-              newvoters.add(email.lower())
-              newvoters.add(realemail.lower())
-              badvoters.discard(email.lower())
-              badvoters.discard(realemail.lower())
+                if realemail in newvoters:
+                    newvoters[realemail]['roles'].append(role)
+                    newvoters[realemail]['emails'].append(email)
+                else:
+                    newvoters[realemail] = {'roles': role, 'emails': email,
+                             'validation': validation}
+                          
+                badvoters.pop(realemail, None)  # A good vote overrides a bad one
         else:
             print 'Fail!', name, email, validation
-            badvoters.add(email.lower())
+            badvoters[email] = email
 
-conn.commit()
+conn.commit()  # Commit votes
+
+# If there were any validation fails, send out the error
+for b in badvoters:
+    sendbadmail(b, info)
+
+# Now, send out emails to successful voters; every time we send an email,
+# commit that it's been done.
+for b in newvoters:
+    sendgoodmail(newvoters[b], info)
+    c.execute('UPDATE voters SET confirmed = 1 WHERE validation = ?', (newvoters[b]['validation']))
+    conn.commit() # Commit this one
+
 conn.close()
-vfile = open('newvoters.txt', 'w')
-vfile.write('\n'.join(newvoters))
-vfile.write('\n')
-vfile.close()
-vfile = open('badvoters.txt', 'w')
-vfile.write('\n'.join(badvoters))
-vfile.write('\n')
-vfile.close()
+
+def sendbadmail(b, info):
+    # TODO: Remove debugging override
+    b = 'david@d2j.us'
+    message = MIMEText(info['badtext'])
+    message['Subject'] = info['badsubj']
+    message['From'] = info['from']
+    message['To'] = b
+    info['s'].sendmail(info['from'], [b], message.as_string())
+
+def sendgoodmail(voter, info):
+    # TODO: Remove debugging override
+    voter['emails'] = ['david@d2j.us', 'election@d4tm.org']
+    if len(voter['roles']) > 1:
+        info['goodtext'] += '\nYour vote has been recorded for each of your %d roles.' % len(voter['roles'])
+    message = MIMEText(info['goodtext'])
+    message['Subject'] = info['goodsubj']
+    message['From'] = info['from']
+    message['To'] = ', '.join(voter['emails'])
+    info['s'].sendmail(info['from'], voter['emails'], message.as_string())
+
